3e7dc332a741c61cdab25e05fcfedd1a297e475a
leo.lopes <devleo.oliveira@gmail.com>
2025-05-09 13:48:45 -0300

    feat: add new columns

--- crud-api.js
+++ crud-api.js
@@ -6,8 +6,8 @@ app.use(bodyParser.json());
 
 // Banco de dados em memória (para teste)
 let users = [
-    { id: 1, name: 'João Silva', email: 'joao@example.com' },
-    { id: 2, name: 'Maria Souza', email: 'maria@example.com' }
+    { id: 1, name: 'João Silva', email: 'joao@example.com', phone: "(11) 1234-5678", address: "Rua A, 123", city: "Cidade A", state: "Estado A", country: "Pais A", zipCode: "12345-678" },
+    { id: 2, name: 'Maria Souza', email: 'maria@example.com', phone: "(21) 9876-5432", address: "Rua B, 456", city: "Cidade B", state: "Estado B", country: "Pais B", zipCode: "54321-987" },
 ];
 
 // Rota de teste
@@ -32,7 +32,13 @@ app.post('/users', (req, res) => {
     const user = {
         id: users.length + 1,
         name: req.body.name,
-        email: req.body.email
+        email: req.body.email,
+        phone: req.body.phone,
+        address: req.body.address,
+        city: req.body.city,
+        state: req.body.state,
+        country: req.body.country,
+        zipCode: req.body.zipCode
     };
     users.push(user);
     const data = {
@@ -49,6 +55,12 @@ app.put('/users/:id', (req, res) => {
 
     user.name = req.body.name;
     user.email = req.body.email;
+    user.phone = req.body.phone;
+    user.address = req.body.address;
+    user.city = req.body.city;
+    user.state = req.body.state;
+    user.country = req.body.country;
+    user.zipCode = req.body.zipCode;
 
     const data = {
         message: 'Usuário atualizado com sucesso!',
